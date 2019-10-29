63ddff34dd0113a158b80138477eaf32f349380c
James Scalise <jamescscalise@gmail.com>
2019-10-29 22:07:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,77 +47,7 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_9
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 9
-    SQL
-    
-    
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
   
-  def self.students_below_12th_grade
-    
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade != 12
-    SQL
-    
-    
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-  
-  def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      ORDER BY students.id ASC
-      LIMIT ?
-    SQL
-    
-    
-    DB[:conn].execute(sql, x).map do |row|
-      self.new_from_db(row)
-    end
-  end
-  
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      ORDER BY students.id ASC
-      LIMIT 1
-    SQL
-    
-    
-    #DB[:conn].execute(sql).map do |row|
-      student = self.new_from_db(DB[:conn].execute(sql)[0])
-   # end
-   #binding.pry
-  end
-  
-  def self.all_students_in_grade_X(x)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = ?
-      ORDER BY students.id ASC
-    SQL
-    
-    
-    DB[:conn].execute(sql, x).map do |row|
-      self.new_from_db(row)
-    end
-  end
   
   def save
     sql = <<-SQL
